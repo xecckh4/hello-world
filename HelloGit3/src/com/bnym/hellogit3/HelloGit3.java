bdc0fe80f7e2810d8a794ad45f591744be45f0e2
Ramesh_xecckh4 <ramesh.thiruvenkataraju@bnymellon.com>
2018-09-06 14:45:11 +0530

    Added Sys Out 2.

--- HelloGit3/src/com/bnym/hellogit3/HelloGit3.java
+++ HelloGit3/src/com/bnym/hellogit3/HelloGit3.java
@@ -3,7 +3,8 @@ package com.bnym.hellogit3;
 public class HelloGit3 {
 
 	public static void main(String[] args) {
-		//System out statements.
+		//System out statements 1.
+		//System out statements 2.
 		System.out.println ("Hello GIT 3.");
 
 	}
