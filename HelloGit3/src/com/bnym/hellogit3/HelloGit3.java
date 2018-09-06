e0c22a4062dde34d6ce80e4a676ed0ec26003d54
Ramesh_xecckh4_2 <ramesh.thiruvenkataraju@bnymellon.com>
2018-09-06 14:59:21 +0530

    Added comments for Sys Out 1.5.

--- HelloGit3/src/com/bnym/hellogit3/HelloGit3.java
+++ HelloGit3/src/com/bnym/hellogit3/HelloGit3.java
@@ -4,6 +4,7 @@ public class HelloGit3 {
 
 	public static void main(String[] args) {
 		//System out statements 1.
+		//System out statement 1.5.
 		//System out statements 2.Please note, this was added later.
 		System.out.println ("Hello GIT 3.");
 
